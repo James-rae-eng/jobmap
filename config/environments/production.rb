ac706393e8931bc028d35ed25961a6ed37db1d1e
James Rae <jrae151515@yahoo.co.uk>
2024-01-19 18:18:59 +0000

    set up basic scaffold with linked pg database to trail with. used this guide https://crisprogramming.hashnode.dev/deploy-ruby-on-rails-application-with-railway

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  
+
+  # Railway domain info
+  config.hosts << ENV['DEVELOPMENT_HOSTS']
 end
